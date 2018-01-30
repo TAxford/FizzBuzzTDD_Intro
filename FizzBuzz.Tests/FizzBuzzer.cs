f7695b8276f0f70e7feab488c46a189d6ac42da6
TAxford <t.axford2@yahoo.com>
2018-01-30 12:53:04 -0500

    Initial

---
+++ FizzBuzz.Tests/FizzBuzzer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Tests
+{
+    class FizzBuzzer
+    {
+        //public string Get(int number)
+        //{
+        //    return number.ToString();
+        //}
+
+        public string Get(int number)
+        {
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+           
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            return number.ToString();
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ FizzBuzz.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Tests
+{
+
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+        //[Test]
+        //public void Get_Returns_A_Number()
+        //{
+        //    var fizzBuzzer = new FizzBuzzer();
+
+        //    var result = fizzBuzzer.Get(2);
+
+        //    Assert.That(result, Is.EqualTo("2"));
+        //}
+
+
+        //[Test]
+        //public void Get_Return_A_Number_For_4()
+        //{
+        //    var fizzBuzzer = new FizzBuzzer();
+
+        //    var result = fizzBuzzer.Get(4);
+
+        //    Assert.That(result, Is.EqualTo("Fizz"));
+        //}
+
+        [Test]
+        public void Get_Returns_Buzz_For_5()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(5);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
